62150f078315a666029938d9ca17cdcfaaddd121
Muhammad Hamdi <prime.energy7@gmail.com>
2018-08-04 01:25:05 +0200

    finished messages CRUD

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -6,4 +6,43 @@ class ContactMessagesController < ApplicationController
   def show
     @message = ContactMessage.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @message = ContactMessage.new(message_params)
+
+    if @message.save
+      redirect_to @message
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @message = ContactMessage.find(params[:id])
+  end
+
+  def update
+    @message = ContactMessage.find(params[:id])
+
+    if @message.update(message_params)
+      redirect_to @message
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message = ContactMessage.find(params[:id])
+    @message.destroy
+
+    redirect_to contact_messages_path
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:first_name, :last_name, :email, :subject, :description)
+    end
 end
